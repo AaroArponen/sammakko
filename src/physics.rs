0e89596ae6ed5460608694a6828e95cc6070a465
Victor VoxWave Bankowski <victor_bankowski@hotmail.com>
2019-01-30 20:39:40 +0200

    Fysiikka systeemi melkein tehty. Pieni ongelma parjoinin kanssa

--- src/physics.rs
+++ src/physics.rs
@@ -1,5 +1,23 @@
-use na::Vector2;
-use specs::{Component};
+use crate::na::{Point2, Vector2};
+use specs::prelude::ParallelIterator;
+use specs::{Component, VecStorage, WriteStorage, System, ParJoin};
 
 #[derive(Component)]
-pub struct Position(Vector2<f64>);
+#[storage(VecStorage)]
+pub struct Position(pub Point2<f64>);
+
+pub struct PhysicsSystem;
+
+impl<'a> System<'a> for PhysicsSystem {
+    type SystemData = (
+        WriteStorage<'a, Position>,
+    );
+
+    fn run(&mut self, mut positions: Self::SystemData) {
+        (&mut positions,)
+            .par_join()
+            .for_each(|pos| {
+                pos.0 += Vector2::new(1,1);
+            });
+    }
+}
