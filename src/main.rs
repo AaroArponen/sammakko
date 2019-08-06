202357c7173178ccc1cd096a493abd4feb7a0e72
Aaro Arponen <aaro.arponen@hotmail.com>
2019-08-06 18:28:07 +0300

    amethyst from the scratch, pong example

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
-#[macro_use]
-extern crate specs_derive;
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
 
-use piston_window::*;
-use nalgebra as na;
+pub struct SammakkoKatedraali;
 
-mod physics;
-mod rendering;
+impl SimpleState for SammakkoKatedraali {}
 
-fn main() {
-    let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
-        .exit_on_esc(true).build().unwrap();
-    while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics| {
-            clear([1.0; 4], graphics);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      context.transform,
-                      graphics);
-        });
-    }
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+    let game_data = GameDataBuilder::default()
+    .with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)
+                    .with_clear([0.9, 0.1, 0.1, 1.0]),
+            )
+            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+
+    let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, SammakkoKatedraali, game_data)?;
+    game.run();
+
+
+    Ok(())
 }
