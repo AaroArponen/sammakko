f7d6e4d9396e1ac04e6331e51010a867db8a58c0
Aaro Arponen <aaro.arponen@hotmail.com>
2019-08-31 17:55:41 +0300

    Amethyst gone, ggez in. Some muzak updated

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,46 @@
-use amethyst::{
-    prelude::*,
-    renderer::{
-        plugins::{RenderFlat2D, RenderToWindow},
-        types::DefaultBackend,
-        RenderingBundle,
-    },
-    utils::application_root_dir,
-};
+use ggez;
+use ggez::event;
+use ggez::graphics;
+use ggez::nalgebra as na;
 
-pub struct SammakkoKatedraali;
+struct MainState {
+    pos_x: f32,
+}
 
-impl SimpleState for SammakkoKatedraali {}
+impl MainState {
+    fn new() -> ggez::GameResult<MainState> {
+        let s = MainState { pos_x: 0.0 };
+        Ok(s)
+    }
+}
 
-fn main() -> amethyst::Result<()> {
-    amethyst::start_logger(Default::default());
-    let app_root = application_root_dir()?;
-    let display_config_path = app_root.join("config").join("display.ron");
-    let game_data = GameDataBuilder::default()
-    .with_bundle(
-        RenderingBundle::<DefaultBackend>::new()
-            // The RenderToWindow plugin provides all the scaffolding for opening a window and drawing on it
-            .with_plugin(
-                RenderToWindow::from_config_path(display_config_path)
-                    .with_clear([0.9, 0.1, 0.1, 1.0]),
-            )
-            // RenderFlat2D plugin is used to render entities with a `SpriteRender` component.
-            .with_plugin(RenderFlat2D::default()),
-    )?;
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut ggez::Context) -> ggez::GameResult {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
 
-    let assets_dir = app_root.join("assets");
-    let mut game = Application::new(assets_dir, SammakkoKatedraali, game_data)?;
-    game.run();
+    fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
 
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            na::Point2::new(self.pos_x, 380.0),
+            100.0,
+            2.0,
+            graphics::WHITE,
+        )?;
+        graphics::draw(ctx, &circle, (na::Point2::new(0.0, 0.0),))?;
 
-    Ok(())
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+
+pub fn main() -> ggez::GameResult { 
+    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
+    let (ctx, event_loop) = &mut cb.build()?;
+    let state = &mut MainState::new()?;
+    event::run(ctx, event_loop, state)
 }
